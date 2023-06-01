5322a891e0aee615a5ec1ce7012b0b1119c85265
Alejandro Pestanas Vargas <isc19350316@gmail.com>
2023-05-31 19:11:34 -0600

    Consulta del clima dinamicamente

--- src/components/WeatherTimeData.jsx
+++ src/components/WeatherTimeData.jsx
@@ -1,38 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+const APIKEY = '73d287dd990c454a82b4c6a5264637ce'
 
 const WeatherTimeData = () => {
-    const APIKEY = '73d287dd990c454a82b4c6a5264637ce'
-    const [weather, setWeather] = useState([])
+    const [currentWeather, setCurrentWeather] = useState([])
+    const [forecastWeather, setForecastWeather] = useState([])
+    const [city, setCity] = useState('')
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(`https://api.weatherbit.io/v2.0/current?city=tuxtepec&lang=es&key=${APIKEY}`)
-            const data = await response.json()
-            // console.log(data.data)
-            setWeather(data.data)
-        }
-
-        fetchData()
-    }, [])
-
+    const searchPressed = () => {
+        fetch(`https://api.weatherbit.io/v2.0/current?city=${city}&lang=es&key=${APIKEY}&include=minutely`)
+            .then((res) => res.json())
+            .then((data) => {
+                setCurrentWeather(data.data)
+                setForecastWeather(data.minutely)
+            })
+    }
 
     return (
-        <div>
+        <div className='mt-4'>
+            <div>
+                <input
+                    className='text-center border border-1 rounded-4 w-25 p-3 '
+                    type="text"
+                    placeholder='Ciudad...'
+                    onChange={(e) => setCity(e.target.value)}
+                />
+                <button
+                    className='btn btn-primary ms-4'
+                    onClick={(searchPressed)}>
+                    Buscar
+                </button>
+            </div>
+
             {
-                weather.map(currentWeather => {
+                currentWeather.map(currentWeather => {
                     return (
                         <div className='row d-flex justify-content-center'>
                             <div className='col-md-4'>
-                                <div className='card mt-3' >
-                                    <div className="card-header">
-                                        Ficha meteorologica
-                                    </div>
+                                <div className='card shadow-lg p-3 mb-5 bg-body-tertiary rounded rounded-5 mt-5' >
                                     <div className='card-body'>
-                                        <h5 className='card-title'>Ciudad : {currentWeather.city_name}</h5>
-                                        <p className='card-text'>Pais : {currentWeather.country_code}</p>
-                                        <p className='card-text'>Fecha : {currentWeather.datetime}</p>
-                                        <p className='card-text'>Temperatura : {currentWeather.temp}°</p>
-                                        <p className='card-text'>Sensación terminca : {currentWeather.app_temp}°</p>
+                                        <h5 className='card-title'>Ahora</h5>
+                                        <hr />
+                                        <h5 className='card-title'>{currentWeather.city_name}, {currentWeather.country_code}</h5>
+                                        <p className='card-text'>Temperatura : {currentWeather.temp}°C</p>
+                                        <p className='card-text'>Sensación terminca : {currentWeather.app_temp}°C</p>
                                         <p className='card-text'>Humedad : {currentWeather.rh}%</p>
                                         <p className='card-text'>Probabilidad de lluvia : {currentWeather.precip}%</p>
                                         <p className='card-text'>Descripción : {currentWeather.weather.description}</p>
@@ -42,6 +52,30 @@ const WeatherTimeData = () => {
                                 </div>
                             </div>
                         </div>
+                    )
+                })
+            }
+
+            {
+                forecastWeather.map(forecastWeather => {
+                    return (
+                        <div className='row d-flex justify-content-center'>
+                            <div>
+                                <h5 className='card-title'>Pronóstico de 1 hora / minuto</h5>
+                                <hr />
+                            </div>
+                            <div className='col-md-4'>
+                                <div className='card shadow-lg p-3 mb-5 bg-body-tertiary rounded rounded-5 mt-5' >
+                                    <div className='card-body'>
+                                        <h5 className='card-title'>{forecastWeather.timestamp_local}</h5>
+                                        <hr />
+                                        <h5 className='card-title'>Temperatura : {forecastWeather.temp}°C</h5>
+                                        <p className='card-text'>Probabilidad de lluvia : {forecastWeather.precip}%</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     )
                 })
             }
